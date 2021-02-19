b99a91f65539d99dcf020783050028f3fa8c7da2
Jarred Capellman <jcapellman@hotmail.com>
2021-02-18 21:43:39 -0600

    Added: Initial work on the FatBinary class

---
+++ MFP.library/Objects/FatBinary.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+using MFP.library.Common;
+using MFP.library.Enums;
+
+namespace MFP.library.Objects
+{
+    public class FatBinary
+    {
+        public static List<MachoBinary> Load(Stream stream)
+        {
+            var binaries = new List<MachoBinary>();
+
+            var eReader = new EndianReader(stream, Endian.BIG);
+
+            var binaryCount = eReader.ReadUInt32();
+            
+            var startPosition = stream.Position;
+
+            for (var idx = 0; idx < binaryCount; idx++)
+            {
+                stream.Seek(startPosition + Constants.FAT_HEADER_SIZE * idx + 8, SeekOrigin.Begin);
+
+                var offset = eReader.ReadInt32();
+
+                var size = eReader.ReadInt32();
+
+                var result = new byte[size];
+
+                stream.Seek(offset, SeekOrigin.Begin);
+
+                stream.Read(result, 0, size);
+
+                binaries.Add(MachoBinary.Load(result));
+            }
+
+            return binaries;
+        }
+    }
+}
