bf5802ac5e67b6838633869293cb6fba68851812
Jarred Capellman <jcapellman@hotmail.com>
2021-02-18 21:38:28 -0600

    Added: FAT_HEADER_SIZE constant

--- MFP.library/Common/Constants.cs
+++ MFP.library/Common/Constants.cs
@@ -2,12 +2,14 @@
 {
     public class Constants
     {
-        public const uint FILEMAGIC_I386 = 0xFEEDFACE;
+        public const uint FILEMAGIC_I386  = 0xFEEDFACE;
 
         public const uint FILEMAGIC_AMD64 = 0xFEEDFACF;
 
         public const uint FILEMAGIC_ARM64 = 0xFEEDFACD;
 
         public const uint FILEMAGIC_MULTI = 0xBEBAFECA;
+
+        public const int FAT_HEADER_SIZE = 20;
     }
 }
