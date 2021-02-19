0ee2cba57ffc7ac659a6806a5a329a8a8d913371
Jarred Capellman <jcapellman@hotmail.com>
2021-02-18 18:55:49 -0600

    Added: Null and Valid Unit Test

---
+++ MFP.UnitTests/MachoReaderTests.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using System.Linq;
+
+using MFP.library;
+using MFP.library.Enums;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MFP.UnitTests
+{
+    [TestClass]
+    public class MachoReaderTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void MachoReader_NullFileName()
+        {
+            var _ = MachoReader.Read(fileName: null);
+        }
+
+        [TestMethod]
+        public void MachoReader_ValidAMD64()
+        {
+            var result = MachoReader.Read(fileName: Path.Combine(AppContext.BaseDirectory, "Samples", "Macho_AMD64"));
+
+            Assert.IsNotNull(result);
+
+            Assert.IsTrue(result.Where(a => a.Format == MachoFormat.AMD64).Count() == 1);
+        }
+    }
+}
