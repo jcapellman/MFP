5103ab97d53c4d38dba9dc444df829fe3b1c287f
Jarred Capellman <jcapellman@hotmail.com>
2021-02-18 21:43:14 -0600

    Added: Initial checkin of the Extensions class

---
+++ MFP.library/Common/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+using MFP.library.Enums;
+
+namespace MFP.library.Common
+{
+    public static class Extensions
+    {
+        public static MachoFormat GetFormat(this BinaryReader bReader)
+        {
+            var magicBytes = bReader.ReadUInt32();
+
+            return magicBytes switch
+            {
+                Constants.FILEMAGIC_I386 => MachoFormat.I386,
+                Constants.FILEMAGIC_AMD64 => MachoFormat.AMD64,
+                Constants.FILEMAGIC_ARM64 => MachoFormat.ARM64,
+                Constants.FILEMAGIC_MULTI => MachoFormat.MULTI,
+                _ => MachoFormat.UNKNOWN,
+            };
+        }
+    }
+}
