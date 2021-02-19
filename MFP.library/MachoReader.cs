637b27f0f2d701e4dbd80e59c579b0bf9d18cc10
Jarred Capellman <jcapellman@hotmail.com>
2021-02-18 18:38:23 -0600

    Added: Initial work on the MachoReader class

---
+++ MFP.library/MachoReader.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+using MFP.library.Common;
+using MFP.library.Enums;
+using MFP.library.Objects;
+
+namespace MFP.library
+{
+    public class MachoReader : IDisposable
+    {
+        private static MachoFormat ParseMagicBytes(uint magicBytes)
+        {
+            return magicBytes switch
+            {
+                Constants.FILEMAGIC_I386 => MachoFormat.I386,
+                Constants.FILEMAGIC_AMD64 => MachoFormat.AMD64,
+                Constants.FILEMAGIC_ARM64 => MachoFormat.ARM64,
+                Constants.FILEMAGIC_MULTI => MachoFormat.MULTI,
+                _ => MachoFormat.UNKNOWN,
+            };
+        }
+
+        public static List<MachoBinary> Read(string fileName)
+        {
+            if (!File.Exists(fileName))
+            {
+                throw new FileNotFoundException("File does exist", fileName);
+            }
+
+            using var fileStream = File.Open(fileName, FileMode.Open, FileAccess.Read);
+
+            return Read(fileStream);
+        }
+
+        public static List<MachoBinary> Read(Stream stream)
+        {
+            using var bReader = new BinaryReader(stream, Encoding.UTF8, true);
+
+            var binaryFormat = ParseMagicBytes(bReader.ReadUInt32());
+
+            switch (binaryFormat)
+            {
+                case MachoFormat.UNKNOWN:
+                    return null;
+                case MachoFormat.MULTI:
+                {
+                    var result = new List<MachoBinary>();
+
+                    // TODO: Handle multiple binaries
+
+                    return result;
+                }
+                case MachoFormat.I386:
+                case MachoFormat.AMD64:
+                case MachoFormat.ARM64:
+                default:
+                    return new List<MachoBinary> { MachoBinary.Load(bReader, binaryFormat) };
+            }
+        }
+        
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
