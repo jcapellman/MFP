50b611dafc848cd84a8c36d72e471988cf2ae5a0
Jarred Capellman <jcapellman@hotmail.com>
2021-02-18 18:37:38 -0600

    Added: Constants class

---
+++ MFP.library/Common/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace MFP.library.Common
+{
+    public class Constants
+    {
+        public const uint FILEMAGIC_I386 = 0xFEEDFACE;
+
+        public const uint FILEMAGIC_AMD64 = 0xFEEDFACF;
+
+        public const uint FILEMAGIC_ARM64 = 0xFEEDFACD;
+
+        public const uint FILEMAGIC_MULTI = 0xBEBAFECA;
+    }
+}
