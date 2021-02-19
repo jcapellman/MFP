3b4f20d63c5e9c0787350455a8935a69714ce7fb
Jarred Capellman <jcapellman@hotmail.com>
2021-02-18 18:39:18 -0600

    Added: Initial work on the MachoBinary class

---
+++ MFP.library/Objects/MachoBinary.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+using MFP.library.Enums;
+
+namespace MFP.library.Objects
+{
+    public class MachoBinary
+    {
+        public MachoFormat Format { get; internal set; }
+
+        public static MachoBinary Load(BinaryReader bReader, MachoFormat binaryFormat)
+        {
+            var result = new MachoBinary { Format = binaryFormat };
+
+            return result;
+        }
+    }
+}
