d3ebdbf09812bb2ed3e48b19af1eaa792d8c11e1
Jarred Capellman <jcapellman@hotmail.com>
2021-02-18 21:42:12 -0600

    Added: Binary overload for MachoBinary.Load

--- MFP.library/Objects/MachoBinary.cs
+++ MFP.library/Objects/MachoBinary.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 
+using MFP.library.Common;
 using MFP.library.Enums;
 
 namespace MFP.library.Objects
@@ -7,6 +8,17 @@ namespace MFP.library.Objects
     public class MachoBinary
     {
         public MachoFormat Format { get; internal set; }
+
+        public static MachoBinary Load(byte[] binary)
+        {
+            using var ms = new MemoryStream(binary);
+
+            var bReader = new BinaryReader(ms);
+
+            var format = bReader.GetFormat();
+
+            return Load(bReader, format);
+        }
 
         public static MachoBinary Load(BinaryReader bReader, MachoFormat binaryFormat)
         {
