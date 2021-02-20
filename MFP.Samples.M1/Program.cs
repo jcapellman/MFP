b97ffabef5ff98759004a21467bffa0eba4e46c4
Jarred Capellman <jcapellman@hotmail.com>
2021-02-19 19:21:44 -0600

    Added: Basic M1 Sample

---
+++ MFP.Samples.M1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MFP.Samples.M1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo apfel m1!");
+        }
+    }
+}
