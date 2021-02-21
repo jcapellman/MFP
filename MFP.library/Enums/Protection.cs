24ef2c68659a7ebae9f1c22004e5058b29e772ba
Jarred Capellman <jcapellman@hotmail.com>
2021-02-21 16:38:18 -0600

    Added: Protection Enum and tweaks to the BaseCommand class

---
+++ MFP.library/Enums/Protection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MFP.library.Enums
+{
+    [Flags]
+    public enum Protection
+    {
+        Read = 1,
+        Write = 2,
+        Execute = 4
+    }
+}
---
+++ MFP.library/Objects/Commands/Base/BaseCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace MFP.library.Objects.Commands.Base
+{
+    public class BaseCommand
+    {
+        internal BaseCommand(BinaryReader bReader, Func<Stream> streamProvider)
+        {
+            StreamProvider = streamProvider;
+
+            BReader = bReader;
+        }
+        
+        protected readonly BinaryReader BReader;
+
+        protected readonly Func<Stream> StreamProvider;
+    }
+}
