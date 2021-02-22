07cb68b9694f0f07ab6f53035e8671c3fa0e47ff
Jarred Capellman <jcapellman@hotmail.com>
2021-02-21 18:09:50 -0600

    Added: Initial work on the Dylib class

---
+++ MFP.library/Objects/Commands/DyLib.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+using MFP.library.Enums;
+using MFP.library.Objects.Commands.Base;
+
+namespace MFP.library.Objects.Commands
+{
+    public class DyLib : BaseCommand
+    {
+        private const int NamePropertyOffset = 24;
+
+        public long TimeStamp { get; private set; }
+
+        public long CurrentVersion { get; private set; }
+        
+        public long CompatibilityVersion { get; private set; }
+        
+        public string Name { get; private set; }
+
+        internal override CommandTypes CommandTypes => CommandTypes.DyLib;
+        
+        internal override BaseCommand InitializeCommand(BinaryReader bReader, Stream stream, uint commandSize)
+        {
+            // TODO: Skip for now
+            bReader.ReadBytes(4);
+
+            TimeStamp = bReader.ReadInt32();
+
+            CurrentVersion = bReader.ReadInt32();
+
+            CompatibilityVersion = bReader.ReadInt32();
+
+            Name = Encoding.UTF8.GetString(bReader.ReadBytes(Convert.ToInt32(commandSize) - NamePropertyOffset)).TrimEnd('\0');
+
+            return this;
+        }
+    }
+}
