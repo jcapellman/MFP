5833a9a2b25dfc58a0d683b22b38b4343e7ba0ad
Jarred Capellman <jcapellman@hotmail.com>
2021-02-22 19:20:05 -0600

    Added: Initial work on the Section class

---
+++ MFP.library/Common/Section.cs
@@ -0,0 +1,15 @@
+﻿namespace MFP.library.Common
+{
+    public sealed class Section
+    {
+        public string Name { get; internal set; }
+
+        public ulong Address { get; internal set; }
+
+        public ulong Size { get; internal set; }
+
+        public uint Offset { get; internal set; }
+
+        public uint AlignExponent { get; internal set; }
+    }
+}
