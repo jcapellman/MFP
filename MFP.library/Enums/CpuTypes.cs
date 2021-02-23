f3244e55cad23c76a7d6373c77ae429cf68dd9fb
Jarred Capellman <jcapellman@hotmail.com>
2021-02-22 20:14:12 -0600

    Added: CpuTypes enumeration

---
+++ MFP.library/Enums/CpuTypes.cs
@@ -0,0 +1,27 @@
+﻿using MFP.library.Common;
+
+namespace MFP.library.Enums
+{
+    public enum CpuTypes : uint
+    {
+        Any = 0,
+        Vax = 1,
+        // 2-5 Skipped
+        Motorolla68K = 6,
+        x86 = 7,
+        x64 = x86 | Constants.ARCHITECTURE64,
+        MIPS = 8,
+        // 9 Skipped
+        MC98000 = 10,
+        HP_PA = 11,
+        ARM = 12,
+        ARM64 = ARM | Constants.ARCHITECTURE64,
+        MC88000 = 13,
+        SPARC = 14,
+        I860 = 15,
+        Alpha = 16,
+        // 17 Skipped
+        PowerPC = 18,
+        PowerPC64 = PowerPC | Constants.ARCHITECTURE64
+    }
+}
