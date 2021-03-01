b95eeaab50b84ec9a51ba8983d10512a8d528499
Jarred Capellman <jcapellman@hotmail.com>
2021-02-28 21:36:27 -0600

    Change: Updated the InitializeCommand and added LoadCommand

--- MFP.library/Objects/Commands/Base/BaseCommand.cs
+++ MFP.library/Objects/Commands/Base/BaseCommand.cs
@@ -9,13 +9,33 @@ namespace MFP.library.Objects.Commands.Base
 {
     public abstract class BaseCommand
     {
+        private BinaryReader _binaryReader;
+
+        private CpuTypes _cpuTypes;
+
+        private Stream _stream;
+
+        private uint _commandSize;
+
         internal abstract CommandTypes[] CommandTypes { get; }
 
-        internal abstract BaseCommand InitializeCommand(BinaryReader bReader, Stream stream, uint commandSize, CpuTypes cpuType);
+        internal BaseCommand InitializeCommand(BinaryReader bReader, Stream stream, uint commandSize, CpuTypes cpuType)
+        {
+            _binaryReader = bReader;
+
+            _stream = stream;
+
+            _commandSize = commandSize;
+
+            _cpuTypes = cpuType;
+
+            return LoadCommand();
+        }
+
+        internal abstract BaseCommand LoadCommand();
 
         internal static List<BaseCommand> AssemblyCommands => typeof(BaseCommand).Assembly.GetTypes()
             .Where(a => a.BaseType == typeof(BaseCommand)).Select(b => (BaseCommand)Activator.CreateInstance(b))
             .ToList();
-
     }
 }
