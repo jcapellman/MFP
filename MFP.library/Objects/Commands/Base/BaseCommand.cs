0041432d26f0b94e29d0d5844e33eb264a948116
Jarred Capellman <jcapellman@hotmail.com>
2021-02-28 21:54:20 -0600

    Added: Wrappers for the BinaryReaders

--- MFP.library/Objects/Commands/Base/BaseCommand.cs
+++ MFP.library/Objects/Commands/Base/BaseCommand.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-
+using System.Text;
+using MFP.library.Common;
 using MFP.library.Enums;
 
 namespace MFP.library.Objects.Commands.Base
@@ -15,7 +16,7 @@ namespace MFP.library.Objects.Commands.Base
 
         private Stream _stream;
 
-        private uint _commandSize;
+        protected uint CommandSize;
 
         internal abstract CommandTypes[] CommandTypes { get; }
 
@@ -25,7 +26,7 @@ namespace MFP.library.Objects.Commands.Base
 
             _stream = stream;
 
-            _commandSize = commandSize;
+            CommandSize = commandSize;
 
             _cpuTypes = cpuType;
 
@@ -33,6 +34,14 @@ namespace MFP.library.Objects.Commands.Base
         }
 
         internal abstract BaseCommand LoadCommand();
+
+        protected string GetString(int byteCount = 16) => Encoding.UTF8.GetString(_binaryReader.ReadBytes(byteCount).TakeWhile(a => a != 0).ToArray());
+
+        protected ulong ReadUnsignedInt() => _cpuTypes.IsCpu64bit() ? _binaryReader.ReadUInt64() : _binaryReader.ReadUInt32();
+
+        protected int ReadInt32() => _binaryReader.ReadInt32();
+
+        protected void SkipBytes(int skipBytes = 4) => _binaryReader.ReadBytes(skipBytes);
 
         internal static List<BaseCommand> AssemblyCommands => typeof(BaseCommand).Assembly.GetTypes()
             .Where(a => a.BaseType == typeof(BaseCommand)).Select(b => (BaseCommand)Activator.CreateInstance(b))
