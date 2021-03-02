3ea696d93d5d34fb15bf166efeae0ed6231bbd39
Jarred Capellman <jcapellman@hotmail.com>
2021-03-01 19:40:22 -0600

    Added: Segment Parsing almost complete

--- MFP.library/Objects/Commands/SegmentCommand.cs
+++ MFP.library/Objects/Commands/SegmentCommand.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 
 using MFP.library.Common;
 using MFP.library.Enums;
@@ -10,23 +12,56 @@ namespace MFP.library.Objects.Commands
     {
         public string Name { get; private set; }
 
-        public ulong Address { get; private set; }
+        public long Address { get; private set; }
 
-        public ulong Size { get; private set; }
+        public long Size { get; private set; }
 
-        public ulong FileOffset { get; private set; }
+        public long FileOffset { get; private set; }
+
+        public long FileSize { get; private set; }
 
         public Protection InitialProtection { get; private set; }
 
         public Protection MaximalProtection { get; private set; }
 
+        public int NumberOfSections { get; private set; }
+
         public ReadOnlyCollection<Section> Sections { get; private set; }
 
         internal override CommandTypes[] CommandTypes => new[] { Enums.CommandTypes.Segment, Enums.CommandTypes.Segment_64 };
 
+        public byte[] Data { get; private set; }
+
         internal override BaseCommand LoadCommand()
         {
             Name = GetString();
+
+            Address = ReadBitAwareInt();
+
+            Size = ReadBitAwareInt();
+
+            FileOffset = ReadBitAwareInt();
+            FileSize = ReadBitAwareInt();
+
+            MaximalProtection = (Protection) ReadInt32();
+            InitialProtection = (Protection)ReadInt32();
+
+            NumberOfSections = ReadInt32();
+
+            ReadInt32();    // TODO: Flags
+
+            if (FileSize > 0)
+            {
+                var streamPosition = _stream.Position;
+
+                _stream.Seek(FileOffset, SeekOrigin.Begin);
+
+                Data = ReadSkipBytes(checked((int)FileSize));
+
+                _stream.Position = streamPosition;
+            }
+
+            var sections = new List<Section>();
 
             return this;
         }
